7ca1455b1a142a74310af217142a6b957d9bd048
ronjanackblad <ronja.nackblad@hotmail.com>
2020-06-04 11:14:40 +0200

    Adds search for advice function and get a random joke function

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,10 +1,19 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
 
-export default function Form() {
+export default (props) => {
     const [searchAdvice, setSearchAdvice] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (searchAdvice) {
+            props.setAdvice(searchAdvice);
+        }
+    }
+
+    const onRandomSubmit = (event) => {
+        event.preventDefault();
+        props.setAdvice('random');
     }
 
     const handleChange = (e) => {
@@ -13,15 +22,19 @@ export default function Form() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input 
-                type='text'
-                name='searchAdvice'
-                value={searchAdvice}
-                placeholder='What do you need advice about?'
-                onChange={handleChange}></input>
-                <button>Get advice</button>
+        <div className="d-flex justify-content-center w-100">
+            <form onSubmit={onSubmit} className="d-flex justify-content-center">
+                <input
+                    type='text'
+                    name='searchAdvice'
+                    value={searchAdvice}
+                    placeholder='Search advice..'
+                    onChange={handleChange}
+                    className="m-2 input-group-text w-50" id="inputGroup-sizing-sm"></input>
+                <button className="m-2 btn btn-success">Get advice</button>
+            </form>
+            <form onSubmit={onRandomSubmit}>
+                <button className="m-2 btn btn-info">Get a random advice</button>
             </form>
         </div>
     )
